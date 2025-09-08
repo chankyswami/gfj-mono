7e06e1ba45b1b18f754852ae1185bc85775b6d65
chankyswami <c.innovator@gmail.com>
2025-09-08 19:34:46 +0530

    added one more port 8081

--- terraform-gem/modules/ec2/main.tf
+++ terraform-gem/modules/ec2/main.tf
@@ -33,6 +33,14 @@ resource "aws_security_group" "this" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "HTTP"
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
