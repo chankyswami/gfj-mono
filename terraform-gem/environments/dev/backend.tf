3331f573bc8bd5ea35e0376917907b5ddc8a532f
chankyswami <35588420+chankyswami@users.noreply.github.com>
2025-11-04 12:20:08 +0530

    Update backend.tf

--- terraform-gem/environments/dev/backend.tf
+++ terraform-gem/environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "gems-of-jaipur-bucket"
+    bucket         = "gems-of-jaipur-bucket-1"
     key            = "dev/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
